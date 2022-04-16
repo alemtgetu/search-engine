ccd8d57dd9148208959b0ce7ab768b3e97b19b80
Alem Getu <alem.t.getu@gmail.com>
2022-04-16 17:27:11 -0400

    clean up and update readme

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # search-engine
 
-# WIP
+## What is it?
+
+Simple python tool to scrap all words from a web page and child pages via a href links(only one level down).
+
+For simplicity sake all the words of the page are stored in memory using dictionary hash. When loading new page previous page's words are cleared out from memory.
+
+Python Libraries Used
+
+- `urllib` - to load, read, and decode html pages using url
+- `beatifulSoup` - to beautify html and get texts
+- `flask` - to build simple api
+
+In addition, it includes a simple UI to demo the application. The UI page is html using bootstrap and jquery.
+
+### How to run the dockerized app
+
+Note: docker and docker-compose are used
+
+1. Build the docker images (api and ui)
+
+   `docker-compose build`
+
+1. Run both api and ui docker containers
+   `docker-compose up`
+
+   api - uses port 5001
+
+   ui - uses port 8080
+
+   Note: if you dont have this ports availabe in the hosts machine, you may edit the docker-compose.yaml file for ports on each services
+
+1. open browser http://localhost:8080
+
+   Enter url for site you would like to scrap words in the url text box (eg https://subslikescript.com/movie/Titanic-120338)
+
+   NOTE: Most website don't allow or want users to use scrapping tools like this, so check the site before trying.
+
+1. Once the page is loaded, you may search for word in the text box shown
--- engine.py
+++ engine.py
@@ -1,16 +1,17 @@
-from inspect import BoundArguments
-from xmlrpc.client import Boolean
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import urlsplit, urlparse
-import re
 
 # url = "http://olympus.realpython.org/profiles"
 # split_url = urlsplit(url)
 # # print(split_url)
 # # will be used as base url for relative paths in <a href>
 # base_url = split_url.scheme+'://'+split_url.netloc
-syncategorematic_words = ["a","an", "is","of", "and", "the", "then", "to", "for", "but", "it", "all"]
+syncategorematic_words = ["a", "an", "is", "of", "and",
+                          "the", "then", "to", "for", "but", "it", "all"]
+
+
 def is_valid_url(url) -> bool:
     """Validate if string is valid url
 
@@ -61,9 +62,9 @@ def get_words(soup_obj: BeautifulSoup) -> list:
     -------
     list
         a list of words from the web page
-    """ 
-    
-    return([w  for w in soup_obj.get_text().replace('\n', ' ').split() if w.lower() not in syncategorematic_words])
+    """
+
+    return([w for w in soup_obj.get_text().replace('\n', ' ').split() if w.lower() not in syncategorematic_words])
 
 
 def get_links(soup_object: BeautifulSoup, url: str) -> list:
@@ -92,59 +93,3 @@ def get_links(soup_object: BeautifulSoup, url: str) -> list:
         else:
             absolute_urls.append(base_url+l['href'])
     return absolute_urls
-
-# (Indexing words)
-# Lets use dictionary for now and add words to the main dictionary
-# each word will be key and the count of the words will be the value
-# page_words_list is a list of found in the page
-# words_dict = {}
-
-
-# def add_words_to_words_dict(page_words_list: list):
-#     """Accepts list of words and add the words to dictionary words_dict{}
-
-#     Each word as a key and the count of the word as value
-#     Parameters
-#     ----------
-#     page_words_list: list
-#         The list of all the words to be added to words_dict{}
-#     """
-#     for w in page_words_list:
-#         # strip word of non-alphanumeric characters
-#         pattern = re.compile('\W')
-#         w = re.sub(pattern, '', w).lower()
-#         # add word to dict
-#         if w in words_dict.keys():
-#             words_dict[w] = words_dict[w]+1
-#         else:
-#             words_dict[w] = 1
-
-
-# base_soup = get_soup(url)
-# base_words = get_words(base_soup)
-# # print(base_words)
-# add_words_to_words_dict(base_words)
-# print(words_dict)
-# get links (a hrefs) from the site
-# some hrefs could be relative path
-# check if the hrefs in a is absolute
-# if not use base_url and create absolute path of that url
-# links = base_soup.select("a")
-# all_absolute_url = []
-# for l in links:
-#     if bool(urlparse(l['href']).netloc):
-#         all_absolute_url.append(l['href'])
-#     else:
-#         all_absolute_url.append(base_url+l['href'])
-# links_url = [base_url+l["href"] for l in links]
-# print(all_absolute_url)
-
-# if there are links in the page
-# lets also scrap all words from those pages (one level down only)
-# for child_url in all_absolute_url:
-#     # .get_text().replace('\n', ' ')
-#     page_words = get_words(get_soup(child_url))
-#     add_words_to_words_dict(page_words)
-
-
-# print(words_dict)
